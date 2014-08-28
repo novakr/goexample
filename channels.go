cf00804b631532fa2915b0d5d392866cad1bb6c4
novakr <jr.zengjr@gmail.com>
2014-08-28 16:39:56 +0800

    xxxxxxxx

--- channels.go
+++ channels.go
@@ -12,4 +12,14 @@ func main() {
 	msg := <-messages
 	fmt.Println(msg)
 
+	messages2 := make(chan string, 2)
+	messages2 <- "buffered"
+	messages2 <- "channel"
+
+	fmt.Println(<-messages2)
+	messages2 <- "aaaaaaaaaaaaaa"
+	//messages2 <- "cccccccccccccc"
+	fmt.Println(<-messages2)
+
+	fmt.Println("bbbbbbbbbbbb")
 }
